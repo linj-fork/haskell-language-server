e87f5a72cb6a5a78bce5192f97652f46c9cb9109
Lin Jian <me@linj.tech>
2025-04-07 14:40:37 +0800

    flake/devShell: add HLS and ghcid

--- flake.nix
+++ flake.nix
@@ -68,6 +68,8 @@
             capstone
             stylish-haskell
             pre-commit
+            hpkgs.haskell-language-server
+            hpkgs.ghcid
             ] ++ lib.optionals (!stdenv.isDarwin)
                    [ # tracy has a build problem on macos.
                      tracy
