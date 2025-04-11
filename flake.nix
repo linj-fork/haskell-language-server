d3a1f04bb5ff08de4823944338c0722ff37aa0b1
Lin Jian <me@linj.tech>
2025-04-11 22:39:51 +0800

    flake: add devShells whose hs deps are from Nixpkgs

--- flake.nix
+++ flake.nix
@@ -102,14 +102,35 @@
           '';
         };
 
+        mkDevShellDepFromNix = hpkgs: hpkgs.shellFor {
+          packages = hpkgs: [ hpkgs.haskell-language-server ];
+          nativeBuildInputs = [
+            hpkgs.cabal-install
+            hpkgs.ghcid
+            hpkgs.haskell-language-server
+            hpkgs.stylish-haskell
+          ] ++ [
+            pkgs.capstone
+            pkgs.nixfmt-rfc-style
+            pkgs.pre-commit
+          ];
+          buildInputs = [ ];
+          withHoogle = true;
+        };
+
       in {
         # Developement shell with only dev tools
         devShells = {
           default = mkDevShell pkgs.haskellPackages;
+          default-nix = mkDevShellDepFromNix pkgs.haskellPackages;
           shell-ghc96 = mkDevShell pkgs.haskell.packages.ghc96;
           shell-ghc98 = mkDevShell pkgs.haskell.packages.ghc98;
           shell-ghc910 = mkDevShell pkgs.haskell.packages.ghc910;
           shell-ghc912 = mkDevShell pkgs.haskell.packages.ghc912;
+          shell-ghc96-nix = mkDevShellDepFromNix pkgs.haskell.packages.ghc96;
+          shell-ghc98-nix = mkDevShellDepFromNix pkgs.haskell.packages.ghc98;
+          shell-ghc910-nix = mkDevShellDepFromNix pkgs.haskell.packages.ghc910;
+          shell-ghc912-nix = mkDevShellDepFromNix pkgs.haskell.packages.ghc912;
         };
 
         packages = { inherit docs; };
